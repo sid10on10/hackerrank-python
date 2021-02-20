90fe0a09a6a6c283caa66eff25a4c5fe3e1e38be
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2021-02-20 23:37:32 +0530

    sets

---
+++ sets/union.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+english_set = set(list(map(int,input().split(" "))))
+m = int(input())
+french_set = set(list(map(int,input().split(" "))))
+
+print(len(english_set.union(french_set)))

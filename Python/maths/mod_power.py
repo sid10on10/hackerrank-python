48ce145444aa595d84b235328f27d15b06af0e49
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2021-02-22 23:51:48 +0530

    python maths

---
+++ Python/maths/mod_power.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+m = int(input())
+
+result = pow(a,b)
+result2 = pow(a,b,m)
+print(result)
+print(result2)
